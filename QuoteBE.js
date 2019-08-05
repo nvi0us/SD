91cb46992f0365f611a89093ba02e6620ddd1a6f
nvi0us <47153831+nvi0us@users.noreply.github.com>
2019-08-05 15:52:51 -0500

    Update QuoteBE.js

--- QuoteBE.js
+++ QuoteBE.js
@@ -9,13 +9,15 @@ var {check} = require('express-validator');
 app.post('/FuelQuote', [
     check('gallon').isNumeric()], (req ,res)=>{
     var gallon = req.body.gallon
+    var date = req.body.date
 });
 
-app.get('/FuelQuote', function(req, res,) {
-    var address = res.send('Address from Database from Profile Page');
-    var date = res.send('date');
-    var price = res.send('price from pricing module');
-    var total = res.send('total price from formula');
+app.query('Quote', functoin(err, rows, fields){
+    app.get('/FuelQuote', function(req, res) {
+        var address = res.send(rows[rows.length].address);
+        var price = res.send((rows[rows.length].price);
+        var total = res.send((rows[rows.length].total);
+    });
 });
 app.query('Quote History', function(err, rows, fields){
     app.get('/', function(req, res){
@@ -23,8 +25,5 @@ app.query('Quote History', function(err, rows, fields){
     });
 });
 
-app.get('/FuelQuote', function(req, res, next) {
-    var price = res.send('calculated price');
-});
 
 module.exports = app;
